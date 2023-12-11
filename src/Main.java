9d1cbaebd425fde48df5229b5781961d60481ce6
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-12-11 09:38:56 -0500

    dfdfg

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pokemon1 = new Pokemon("Squirtle", "Water", 5);
+        Pokemon pokemon2 = new Pokemon("Pikachu");
+        System.out.println(pokemon1.toString());
+
+        pokemon2.setName("XXXTENTACION");
+        System.out.println(pokemon2.getName());
+    }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,5 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Pokemon {
+    //class attributes {variables/fields}
+    private String name;
+    private String type;
+    private int level;
+
+    //Constructors - Build objects
+    public Pokemon(String name) {
+        this.name = name;
+        type = "normal";
+        level = 1;
+    }
+
+    public Pokemon(String name, String type, int level){
+        this.name = name;
+        this.type = type;
+        if (level > 0 && level <= 100) {
+            this.level = level;
+        } else {
+            this.level = 1;
+        }
+    }
+
+    //Accessor Methods (Getters) - Return data
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String toString(){
+        String s = ("Name: "+ name+ "\nType: "+ type+ "\nLevel: "+ level);
+        return s;
+    }
+
+    //Mutator Methods (setters) - void methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+       this.type = type;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 }
